5625df0bbf5805e084fb54e957f7c920b772c94d
Shahed Amer <chanyeol6521354@gmail.com>
2024-04-05 16:37:17 +0200

    in de web de routs toegevoegd van de edit en create

--- routes/web.php
+++ routes/web.php
@@ -27,12 +27,16 @@ Route::get('/dashboard', function () {
 //     return view('packages.pakketOverzicht');
 // })->middleware(['auth', 'verified'])->name('packages');
 Route::get('/packages', [PackageController::class, 'index'])->middleware(['auth', 'verified'])->name('packages');
+Route::get('packages/create', [PackageController::class, 'create'])->middleware(['auth', 'verified'])->name('packages.create');
+Route::get('/packages/{id}/edit', [PackageController::class, 'edit'])->middleware(['auth', 'verified'])->name('packages.edit');
+
+Route::delete('/packages/{id}', [PackageController::class, 'destroy'])->middleware(['auth', 'verified'])->name('packages.destroy');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
- 
+
 
 require __DIR__ . '/auth.php';
