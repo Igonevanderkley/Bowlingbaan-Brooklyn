743ece601eab8a57fa711af935eb5419c5b2ca9f
Igone van_der_Kley <igone@vdkley.nl>
2024-04-05 16:09:54 +0200

    migration altered beacause of error

---
+++ app/Models/Player.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Player extends Model
+{
+    use HasFactory;
+
+    public function score()
+    {
+        return $this->hasMany(Score::class, 'playerId');
+    }
+
+    public function reservation()
+    {
+        return $this->hasOne(Reservation::class, 'reservationId');
+    }
+}
