8c2451d4e7e991b3f75248e8adec6d7aa0191463
Igone van_der_Kley <igone@vdkley.nl>
2024-04-05 16:39:03 +0200

    comentaar

--- routes/web.php
+++ routes/web.php
@@ -68,18 +68,6 @@ Route::get('/leaderboard', function () {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
