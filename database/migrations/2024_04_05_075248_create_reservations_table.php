522b4d96749e8884f8a94fc0d0321d756787c8a4
Quinty <quintydewinter1@gmail.com>
2024-04-05 10:33:48 +0200

    migrations toegevoegd

--- database/migrations/2024_04_05_075248_create_reservations_table.php
+++ database/migrations/2024_04_05_075248_create_reservations_table.php
@@ -11,15 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('reservations', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('mobile');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->foreignId('userId');
+            $table->integer('adults');
+            $table->integer('children')->nullable();
+            $table->foreignId('packageId')->nullable();
+            $table->bool('fence');
+            $table->datetime('date');
         });
     }
 
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('reservations');
     }
 };
