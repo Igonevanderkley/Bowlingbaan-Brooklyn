42d23a15054374f33316d0660d888c8074bf5198
Shahed Amer <chanyeol6521354@gmail.com>
2024-04-08 09:06:52 +0200

    Packages routes toegevoegd

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,14 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\PackageController;
+use App\Http\Controllers\AccountOverzichtController;
 use App\Models\Player;
 use App\Models\Score;
 use App\Models\Reservation;
 use Illuminate\Support\Facades\DB;
-
-
-
-
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AccountOverzichtController;
-
 use function Laravel\Prompts\select;
 use App\Http\Controllers\ScoresController;
 
@@ -36,6 +33,12 @@ Route::get('/reservations/{reservation}/edit', [ReservationController::class, 'e
 Route::delete('/reservations/{reservation}', [ReservationController::class, 'destroy'])->name('destroy');
 Route::put('/reservations/{reservation}', [ReservationController::class, 'update'])->name('update');
 
+Route::get('/packages', [PackageController::class, 'index'])->middleware(['auth', 'verified'])->name('packages');
+Route::get('packages/create', [PackageController::class, 'create'])->middleware(['auth', 'verified'])->name('packages.create');
+Route::get('/packages/{id}/edit', [PackageController::class, 'edit'])->middleware(['auth', 'verified'])->name('packages.edit');
+
+Route::delete('/packages/{id}', [PackageController::class, 'destroy'])->middleware(['auth', 'verified'])->name('packages.destroy');
+
 Route::middleware('auth')->group(function () {
     Route::get('/account/edit/{id}', [AccountOverzichtController::class, 'edit'])->name('account.edit');
     Route::put('/account/update/{id}', [AccountOverzichtController::class, 'update'])->name('account.update');
@@ -45,4 +48,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
