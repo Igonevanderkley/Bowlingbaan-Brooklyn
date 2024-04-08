fc0a08f15a2329672abdf218908921b2ea2e6a2c
Quinty <quintydewinter1@gmail.com>
2024-04-08 15:49:12 +0200

    einde dag

--- app/Http/Controllers/MijnReserveringenController.php
+++ app/Http/Controllers/MijnReserveringenController.php
@@ -1,15 +1,26 @@
 <?php
 
+
 namespace App\Http\Controllers;
+
 use App\Models\Reservering;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class MijnReserveringenController extends Controller
 {
     public function mijnReserveringen()
-{
-    $mijnReserveringen = Reservering::all(); // Of gebruik een query om de reserveringen op te halen
-    return view('account.mijnReserveringen', compact('mijnReserveringen'));
+    {
+        // Haal de reserveringen op die aan de persoon Mazin Jamil zijn gekoppeld
+        $mijnReserveringen = Reservering::join('persoons', 'reserverings.persoonId', '=', 'persoons.id')
+                                        ->where('persoons.voorNaam', 'Mazin')
+                                        ->where('persoons.achterNaam', 'Jamil')
+                                        ->orderBy('reserverings.datum', 'desc')
+                                        ->get();
+
+        // Geef de reserveringen door aan de view
+        return view('account.mijnReserveringen', compact('mijnReserveringen'));
+    }
 }
-}
+
 
--- resources/views/account/mijnReserveringen.blade.php
+++ resources/views/account/mijnReserveringen.blade.php
@@ -1,12 +1,23 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-black leading-tight">
-            {{ __('Persoonlijke gegevens') }}
+            {{ __('Mijn Reserveringen') }}
         </h2>
     </x-slot>
 
     @section('content')
         <h1>Overzicht Reserveringen</h1>
+
+        <!-- Toon validatiefouten -->
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
 
         <form action="{{ route('mijnReserveringen') }}" method="GET">
             <label for="vanafDatum">Vanaf Datum:</label>
@@ -17,16 +28,26 @@
         <table>
             <thead>
                 <tr>
+                    <th>Reserveringsnummer</th>
                     <th>Datum</th>
                     <th>Aantal Uren</th>
+                    <th>Begin Tijd</th>
+                    <th>Eind Tijd</th>
+                    <th>Aantal Volwassenen</th>
+                    <th>Aantal Kinderen</th>
                     <!-- Voeg hier andere kolommen toe die je wilt tonen -->
                 </tr>
             </thead>
             <tbody>
             @foreach($mijnReserveringen as $reservering)
                 <tr>
+                    <td>{{ $reservering->reserveringsNummer }}</td>
                     <td>{{ $reservering->datum }}</td>
                     <td>{{ $reservering->aantalUren }}</td>
+                    <td>{{ $reservering->beginTijd }}</td>
+                    <td>{{ $reservering->eindTijd }}</td>
+                    <td>{{ $reservering->aantalVolwassen }}</td>
+                    <td>{{ $reservering->aantalKinderen }}</td>
                     <!-- Voeg hier andere kolommen toe die je wilt tonen -->
                 </tr>
             @endforeach
