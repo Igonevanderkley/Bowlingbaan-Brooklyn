74c877b05042c28e9264a979c97dd52b24b45c42
Quinty <quintydewinter1@gmail.com>
2024-04-08 11:43:11 +0200

    migrations toegevoegd

---
+++ database/migrations/2024_04_08_092933_create_persoon_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('persoon', function (Blueprint $table) {
+            $table->id();
+            $table->string('voorNaam');
+            $table->string('tussenVoegsel')->nullable();
+            $table->string('achterNaam');
+            $table->string('roepNaam');
+            $table->boolean('isVolwassen');
+            $table->boolean('isActief');
+            $table->string('opmerkingen')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('persoon');
+    }
+};
