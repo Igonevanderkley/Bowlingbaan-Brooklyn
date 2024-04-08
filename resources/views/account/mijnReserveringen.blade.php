f4611012a24e75ce44e35042d79a67291a80ce80
Quinty <quintydewinter1@gmail.com>
2024-04-08 13:50:47 +0200

    error gevonden

--- resources/views/account/mijnReserveringen.blade.php
+++ resources/views/account/mijnReserveringen.blade.php
@@ -1,11 +1,9 @@
-
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-black leading-tight">
-            {{ __('Bewerk je gegevens') }}
+            {{ __('Mijn Reserveringen') }}
         </h2>
     </x-slot>
-
 
     @section('content')
         <h1>Mijn Reserveringen</h1>
@@ -21,36 +19,15 @@
             <input type="date" id="vanafDatum" name="vanafDatum">
             <button type="submit">Toon reserveringen</button>
         </form>
-        
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-black leading-tight">
-            {{ __('Bewerk je gegevens') }}
-        </h2>
-    </x-slot>
 
-
-    @extends('layouts.app')
-
-@section('content')
-    <h1>Mijn Reserveringen</h1>
-
-    @if(session('error'))
-        <div class="alert alert-danger">
-            {{ session('error') }}
-        </div>
-    @endif
-
-    <form method="GET" action="{{ route('mijnReserveringen') }}">
-        <label for="vanafDatum">Vanaf datum:</label>
-        <input type="date" id="vanafDatum" name="vanafDatum">
-        <button type="submit">Toon reserveringen</button>
-    </form>
-
-    
-@endsection
-    
-</x-app-layout>
-
+        @if(isset($reserveringen))
+            <!-- Hier kun je je reserveringen tonen -->
+            @foreach ($reserveringen as $reservering)
+                {{ $reservering->datum }} <!-- Voorbeeldweergave van een reservering -->
+            @endforeach
+        @else
+            <p>Geen reserveringen gevonden.</p>
+        @endif
     @endsection
 </x-app-layout>
+
