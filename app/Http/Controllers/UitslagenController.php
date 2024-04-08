a6db0fd1a5a8fffc69c97ad646be05879235af17
Shahed Amer <chanyeol6521354@gmail.com>
2024-04-08 11:36:14 +0200

    een controller gemaakt voor de uitslagen

---
+++ app/Http/Controllers/UitslagenController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Uitslagen;
+use Illuminate\Http\Request;
+
+class UitslagenController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Uitslagen $uitslagen)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Uitslagen $uitslagen)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Uitslagen $uitslagen)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Uitslagen $uitslagen)
+    {
+        //
+    }
+}
