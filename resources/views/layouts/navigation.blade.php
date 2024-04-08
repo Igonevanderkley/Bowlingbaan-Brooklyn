7bfa07d258341a48591ae7ddbbc3585ba2c79c1f
Shahed Amer <chanyeol6521354@gmail.com>
2024-04-08 11:35:03 +0200

    Uitslagen in de nav toegevoegd

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -27,6 +27,12 @@
                     <x-nav-link :href="route('account')" :active="request()->routeIs('account')">
                         {{ __('Account') }}
                     </x-nav-link>
+
+                    <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                        <x-nav-link :href="route('account')" :active="request()->routeIs('account')">
+                            {{ __('Uitslagen') }}
+                        </x-nav-link>
+                    </div>
                 </div>
             </div>
 
@@ -37,7 +43,7 @@
                         <button
                             class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
                             <div>{{ Auth::user()->name }}</div>
-                            
+
 
                             <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
