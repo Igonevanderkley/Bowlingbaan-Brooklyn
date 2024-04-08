2d8b3ce63eacb274505b0221db253b3dcf61d13f
Igone van_der_Kley <igone@vdkley.nl>
2024-04-08 15:31:23 +0200

    seeder altered

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,6 +9,8 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use App\Models\Reservering;
 use App\Models\type_persoon;
+use App\Models\baan;
+
 use App\Models\persoon;
 
 use Faker\Factory as Faker;
@@ -22,49 +24,60 @@ class DatabaseSeeder extends Seeder
     {
         $faker = Faker::create();
 
-        persoon::create(
-            [
-                'typePersoonId' => 1,
-                'voornaam' => 'Mazin',
-                'achternaam' => 'Jamil',
-                'roepnaam' => 'Mazin',
-                'isVolwassen' => 1,
-            ]);
+        persoon::create([
 
-        Reservering::create(
+            'typePersoonId' => 1,
+            'voornaam' => 'Mazin',
+            'achternaam' => 'Jamil',
+            'roepnaam' => 'Mazin',
+            'isVolwassen' => 1,
+
+        ]);
+
+        persoon::create([
+            'typePersoonId' => 1,
+            'voornaam' => 'Arjan',
+            'tussenvoegsel' => 'de',
+            'achternaam' => 'Ruijter',
+            'roepnaam' => 'Arjan',
+            'isVolwassen' => 1,
+        ]);
+
+        Reservering::insert([
             [
                 'persoonId' => 1,
                 'openingsTijdId' => 1,
                 'tariefId' => 1,
-                'baanId' => 8,
+                'baanId' => 5,
                 'pakketOptieId' => 1,
                 'reserveringsStatusId' => 1,
                 'reserveringsNummer' => 2022122000001,
                 'datum' => '2022-12-20',
                 'aantalUren' => 1,
-                'beginTijd' => $faker->time(),
-                'eindTijd' => $faker->time(),
+                'beginTijd' => '15:00:00',
+                'eindTijd' => '16:00:00',
                 'aantalVolwassenen' => 4,
                 'aantalKinderen' => 2
             ],
             [
-                'persoonId' => 2,
+                'persoonId' => 1,
                 'openingsTijdId' => 2,
                 'tariefId' => 2,
-                'baanId' => 8,
+                'baanId' => 2,
                 'pakketOptieId' => 1,
                 'reserveringsStatusId' => 1,
                 'reserveringsNummer' => 2022122000002,
                 'datum' => '2022-12-20',
                 'aantalUren' => 1,
-                'beginTijd' => $faker->time(),
-                'eindTijd' => $faker->time(),
+                'beginTijd' => '15:00:00',
+                'eindTijd' => '16:00:00',
                 'aantalVolwassenen' => 4,
-                'aantalKinderen' => 2
+                'aantalKinderen' => 0
             ]
-        );
+        ]);
 
-        type_persoon::create(
+
+        type_persoon::insert([
             [
                 'id' => 1,
                 'naam' => 'klant',
@@ -77,6 +90,18 @@ class DatabaseSeeder extends Seeder
                 'id' => 3,
                 'naam' => 'gast',
             ]
-        );
+        ]);
+
+        baan::insert([
+            ['id' => 1, 'nummer' => 1, 'heeftHek' => 0],
+            ['id' => 2, 'nummer' => 2, 'heeftHek' => 0],
+            ['id' => 3, 'nummer' => 3, 'heeftHek' => 0],
+            ['id' => 4, 'nummer' => 4, 'heeftHek' => 0],
+            ['id' => 5, 'nummer' => 5, 'heeftHek' => 0],
+            ['id' => 6, 'nummer' => 6, 'heeftHek' => 0],
+            ['id' => 7, 'nummer' => 7, 'heeftHek' => 1],
+            ['id' => 8, 'nummer' => 8, 'heeftHek' => 1]
+
+        ]);
     }
 }
