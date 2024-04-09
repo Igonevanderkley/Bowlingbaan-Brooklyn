cde0013e5153a93696a281706c222469ca0d071b
Quinty <quintydewinter1@gmail.com>
2024-04-09 16:30:47 +0200

    test

--- app/Models/persoon.php
+++ app/Models/persoon.php
@@ -5,20 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class persoon extends Model
+class Persoon extends Model
 {
     use HasFactory;
-
-    protected $table = 'persoon';
-
-    public function type_persoon()
-    {
-        return $this->hasOne(type_persoon::class, 'typePersoonId');
-    }
-
-    public function baan()
-    {
-        return $this->hasOne(baan::class, 'baanId');
-    }
-
 }
