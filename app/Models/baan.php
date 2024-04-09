2899b31d5d3bb82c6098e3afbdc6bc6fb5da9d4d
Quinty <quintydewinter1@gmail.com>
2024-04-09 16:33:22 +0200

    test

--- app/Models/baan.php
+++ app/Models/baan.php
@@ -5,7 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Baan extends Model
+class baan extends Model
 {
     use HasFactory;
+
+    protected $table = 'baan';
+
+    public function persoon()
+    {
+        return $this->hasMany(persoon::class, 'baanId');
+    }
+
 }
--- app/Models/persoon.php
+++ app/Models/persoon.php
@@ -5,7 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Persoon extends Model
+class persoon extends Model
 {
     use HasFactory;
+
+    protected $table = 'persoon';
+
+    public function type_persoon()
+    {
+        return $this->hasOne(type_persoon::class, 'typePersoonId');
+    }
+
+    public function baan()
+    {
+        return $this->hasOne(baan::class, 'baanId');
+    }
+
 }
