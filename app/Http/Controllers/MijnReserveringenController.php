6ae72e1d0bbfc28f344f081bca4953ab6cf8914c
Quinty <quintydewinter1@gmail.com>
2024-04-09 15:54:06 +0200

    user story 1 senario 1 en 2 af

--- app/Http/Controllers/MijnReserveringenController.php
+++ app/Http/Controllers/MijnReserveringenController.php
@@ -9,18 +9,32 @@ use Illuminate\Support\Facades\Validator;
 
 class MijnReserveringenController extends Controller
 {
-    public function mijnReserveringen()
+    public function mijnReserveringen(Request $request)    
     {
+         // Valideer het request
+         $validator = Validator::make($request->all(), [
+            'vanafDatum' => 'nullable|date',
+        ]);
+    
+        // Als validatie mislukt, stuur terug naar vorige pagina met foutmeldingen
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+    
+        // Haal de vanaf datum op uit het request of gebruik een standaardwaarde
+        $vanafDatum = $request->input('vanafDatum', now()->subMonths(1)->format('Y-m-d'));
+    
         // Haal de reserveringen op die aan de persoon Mazin Jamil zijn gekoppeld
         $mijnReserveringen = Reservering::join('persoons', 'reserverings.persoonId', '=', 'persoons.id')
                                         ->where('persoons.voorNaam', 'Mazin')
                                         ->where('persoons.achterNaam', 'Jamil')
+                                        ->where('reserverings.datum', '>=', $vanafDatum)
                                         ->orderBy('reserverings.datum', 'desc')
                                         ->get();
-
-        // Geef de reserveringen door aan de view
-        return view('account.mijnReserveringen', compact('mijnReserveringen'));
+        if ($mijnReserveringen->isEmpty()) {
+            // Toon de view met de reserveringen
+            return redirect()->back()->withErrors(['Er zijn geen reserveringen gevonden voor Mazin Jamil vanaf de opgegeven datum.'])->withInput();
+        }
+        return view('account.mijnReserveringen', compact('mijnReserveringen', 'vanafDatum'));
     }
 }
-
-
--- resources/views/account/mijnReserveringen.blade.php
+++ resources/views/account/mijnReserveringen.blade.php
@@ -1,12 +1,9 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-black leading-tight">
-            {{ __('Mijn Reserveringen') }}
+            {{ __('Overzicht Reserveringen van Mazin Jamil') }}
         </h2>
     </x-slot>
-
-    @section('content')
-        <h1>Overzicht Reserveringen</h1>
 
         <!-- Toon validatiefouten -->
         @if ($errors->any())
@@ -19,39 +16,48 @@
             </div>
         @endif
 
-        <form action="{{ route('mijnReserveringen') }}" method="GET">
+        <!-- Toon foutmelding als er geen reserveringen zijn -->
+        @if(session('error'))
+            <div class="alert alert-danger">
+                {{ session('error') }}
+            </div>
+        @endif
+
+        <form class="w-full text-sm text-left rtl:text-right text-black dark:text-white-400 bg-white" action="{{ route('mijnReserveringen') }}" method="GET">
+            @csrf
             <label for="vanafDatum">Vanaf Datum:</label>
-            <input type="date" id="vanafDatum" name="vanafDatum" value="{{ old('vanafDatum', now()->subMonths(1)->format('Y-m-d')) }}">
-            <button type="submit">Toon Reserveringen</button>
+            <input type="date" id="vanafDatum" name="vanafDatum" value="{{ $vanafDatum }}">
+            <button type="submit" class="btn btn-outline-info">Toon Reserveringen</button>
         </form>
 
-        <table>
-            <thead>
-                <tr>
-                    <th>Reserveringsnummer</th>
-                    <th>Datum</th>
-                    <th>Aantal Uren</th>
-                    <th>Begin Tijd</th>
-                    <th>Eind Tijd</th>
-                    <th>Aantal Volwassenen</th>
-                    <th>Aantal Kinderen</th>
-                    <!-- Voeg hier andere kolommen toe die je wilt tonen -->
-                </tr>
-            </thead>
+        @if(!$mijnReserveringen->isEmpty())     
+        <table class="w-full text-sm text-left rtl:text-right text-black dark:text-white-400 bg-white">
+                <thead>
+                    <tr>
+                        <th>Naam</th>
+                        <th>Datum</th>
+                        <th>Aantal Uren</th>
+                        <th>Begin Tijd</th>
+                        <th>Eind Tijd</th>
+                        <th>Aantal Volwassenen</th>
+                        <th>Aantal Kinderen</th>
+                        <!-- Voeg hier andere kolommen toe die je wilt tonen -->
+                    </tr>
+                </thead>
             <tbody>
             @foreach($mijnReserveringen as $reservering)
-                <tr>
-                    <td>{{ $reservering->reserveringsNummer }}</td>
-                    <td>{{ $reservering->datum }}</td>
-                    <td>{{ $reservering->aantalUren }}</td>
-                    <td>{{ $reservering->beginTijd }}</td>
-                    <td>{{ $reservering->eindTijd }}</td>
-                    <td>{{ $reservering->aantalVolwassen }}</td>
-                    <td>{{ $reservering->aantalKinderen }}</td>
-                    <!-- Voeg hier andere kolommen toe die je wilt tonen -->
-                </tr>
+            <tr>
+                <td>{{ $reservering->voorNaam }} {{ $reservering->achterNaam }}</td>
+                <td>{{ $reservering->datum }}</td>
+                <td>{{ $reservering->aantalUren }}</td>
+                <td>{{ $reservering->beginTijd }}</td>
+                <td>{{ $reservering->eindTijd }}</td>
+                <td>{{ $reservering->aantalVolwassen }}</td>
+                <td>{{ $reservering->aantalKinderen }}</td>
+                <!-- Voeg hier andere kolommen toe die je wilt tonen -->
+            </tr>
             @endforeach
-            </tbody>
-        </table>
-    @endsection
+        </tbody>
+    </table>
+    @endif
 </x-app-layout>
